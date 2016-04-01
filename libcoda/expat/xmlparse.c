a02a7809a21009085f678f4a012574f29a4e0a0f
Sander Niemeijer <niemeijer@stcorp.nl>
2016-04-01 17:31:58 +0200

    Fix C89 incompatibility (variable declarations need to be at the start of a block)

--- libcoda/expat/xmlparse.c
+++ libcoda/expat/xmlparse.c
@@ -1686,12 +1686,14 @@ XML_GetBuffer(XML_Parser parser, int len)
 
   if (len > bufferLim - bufferEnd) {
     int neededSize = len + (int)(bufferEnd - bufferPtr);
+#ifdef XML_CONTEXT_BYTES
+    int keep = (int)(bufferPtr - buffer);
+#endif  /* defined XML_CONTEXT_BYTES */
     if (neededSize < 0) {
       errorCode = XML_ERROR_NO_MEMORY;
       return NULL;
     }
 #ifdef XML_CONTEXT_BYTES
-    int keep = (int)(bufferPtr - buffer);
 
     if (keep > XML_CONTEXT_BYTES)
       keep = XML_CONTEXT_BYTES;
