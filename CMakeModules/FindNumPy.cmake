1bcf2fd79a6a1aec60848e8049a9e3190da5c24e
Sander Niemeijer <niemeijer@stcorp.nl>
2016-05-04 13:25:49 +0200

    Revised CMake tests for external libraries. They now use <package>_LIBRARY_DIR and <package>_INCLUDE_DIR as CMake cache variables (and no longer look at environment variables for lib/include paths). Renamed CMake variable CODA_DISABLE_HDF4_VDATA_ATTRIBUTES to CODA_ENABLE_HDF4_VDATA_ATTRIBUTES (with appropriate reverse semantics) Set the default CMake build type to RelWithDebInfo (instead of defining -NDEBUG for the 'Release' build type)

--- CMakeModules/FindNumPy.cmake
+++ CMakeModules/FindNumPy.cmake
@@ -8,16 +8,7 @@
 
 execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; import os; p = [p for p in sys.path if os.path.exists(os.path.join(p, 'numpy'))]; sys.stdout.write(os.path.join(p[0], 'numpy') if len(p) > 0 else '')" OUTPUT_VARIABLE NUMPY_INSTALL_PREFIX)
 
-find_path(NUMPY_INCLUDE_DIR numpy/ndarrayobject.h
-  PATHS ${NUMPY_INCLUDE} "${NUMPY_INSTALL_PREFIX}/core/include" ENV NUMPY_INCLUDE
-  NO_DEFAULT_PATH)
+find_path(NUMPY_INCLUDE_DIR numpy/ndarrayobject.h PATHS "${NUMPY_INSTALL_PREFIX}/core/include" NO_DEFAULT_PATH)
 
-set(NUMPY_INCLUDE ${NUMPY_INCLUDE} CACHE STRING "Location of Python numpy include files" FORCE)
-
-
-# handle the QUIETLY and REQUIRED arguments and set NUMPY_FOUND to
-# TRUE if all listed variables are TRUE
-#
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(NUMPY DEFAULT_MSG NUMPY_INCLUDE_DIR)
-mark_as_advanced(NUMPY_INCLUDE_DIR)
