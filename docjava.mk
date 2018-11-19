4064205819bf736c95c47d086184673c8782f5e2
Sander Niemeijer <niemeijer@stcorp.nl>
2018-11-19 14:52:30 +0100

    Add generated documentation to repository
    This allows browsing of the full documentation on github.
    But it also removes one less build step dependency for CMake builds on Windows
    
    Also update applicable doxygen version to 1.8.14

--- docjava.mk
+++ docjava.mk
@@ -1,9 +1,5 @@
 JAVA_DOCFILES = \
 	doc/html/java/annotated.html \
-	doc/html/java/arrowdown.png \
-	doc/html/java/arrowright.png \
-	doc/html/java/bc_s.png \
-	doc/html/java/bdwn.png \
 	doc/html/java/class_array_ordering_enum.html \
 	doc/html/java/class_format_enum.html \
 	doc/html/java/class_native_type_enum.html \
@@ -22,31 +18,12 @@ JAVA_DOCFILES = \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_product.html \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type-members.html \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type.html \
-	doc/html/java/closed.png \
-	doc/html/java/doc.png \
 	doc/html/java/dynsections.js \
-	doc/html/java/folderclosed.png \
-	doc/html/java/folderopen.png \
 	doc/html/java/functions.html \
 	doc/html/java/functions_func.html \
 	doc/html/java/index.html \
-	doc/html/java/jquery.js \
-	doc/html/java/nav_f.png \
-	doc/html/java/nav_g.png \
-	doc/html/java/nav_h.png \
-	doc/html/java/open.png \
-	doc/html/java/splitbar.png \
-	doc/html/java/sync_off.png \
-	doc/html/java/sync_on.png
+	doc/html/java/jquery.js
 doc/html/java/annotated.html:
-	$(MAKE) java_doc
-doc/html/java/arrowdown.png:
-	$(MAKE) java_doc
-doc/html/java/arrowright.png:
-	$(MAKE) java_doc
-doc/html/java/bc_s.png:
-	$(MAKE) java_doc
-doc/html/java/bdwn.png:
 	$(MAKE) java_doc
 doc/html/java/class_array_ordering_enum.html:
 	$(MAKE) java_doc
@@ -84,15 +61,7 @@ doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type-members.html:
 	$(MAKE) java_doc
 doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type.html:
 	$(MAKE) java_doc
-doc/html/java/closed.png:
-	$(MAKE) java_doc
-doc/html/java/doc.png:
-	$(MAKE) java_doc
 doc/html/java/dynsections.js:
-	$(MAKE) java_doc
-doc/html/java/folderclosed.png:
-	$(MAKE) java_doc
-doc/html/java/folderopen.png:
 	$(MAKE) java_doc
 doc/html/java/functions.html:
 	$(MAKE) java_doc
@@ -101,18 +70,4 @@ doc/html/java/functions_func.html:
 doc/html/java/index.html:
 	$(MAKE) java_doc
 doc/html/java/jquery.js:
-	$(MAKE) java_doc
-doc/html/java/nav_f.png:
-	$(MAKE) java_doc
-doc/html/java/nav_g.png:
-	$(MAKE) java_doc
-doc/html/java/nav_h.png:
-	$(MAKE) java_doc
-doc/html/java/open.png:
-	$(MAKE) java_doc
-doc/html/java/splitbar.png:
-	$(MAKE) java_doc
-doc/html/java/sync_off.png:
-	$(MAKE) java_doc
-doc/html/java/sync_on.png:
 	$(MAKE) java_doc
--- doclibcoda.mk
+++ doclibcoda.mk
@@ -1,13 +1,5 @@
 LIBCODA_DOCFILES = \
-	doc/html/libcoda/arrowdown.png \
-	doc/html/libcoda/arrowright.png \
-	doc/html/libcoda/bc_s.png \
-	doc/html/libcoda/bdwn.png \
-	doc/html/libcoda/closed.png \
-	doc/html/libcoda/doc.png \
 	doc/html/libcoda/dynsections.js \
-	doc/html/libcoda/folderclosed.png \
-	doc/html/libcoda/folderopen.png \
 	doc/html/libcoda/group__coda__cursor.html \
 	doc/html/libcoda/group__coda__error.html \
 	doc/html/libcoda/group__coda__expression.html \
@@ -17,31 +9,8 @@ LIBCODA_DOCFILES = \
 	doc/html/libcoda/group__coda__types.html \
 	doc/html/libcoda/index.html \
 	doc/html/libcoda/jquery.js \
-	doc/html/libcoda/modules.html \
-	doc/html/libcoda/nav_f.png \
-	doc/html/libcoda/nav_g.png \
-	doc/html/libcoda/nav_h.png \
-	doc/html/libcoda/open.png \
-	doc/html/libcoda/splitbar.png \
-	doc/html/libcoda/sync_off.png \
-	doc/html/libcoda/sync_on.png
-doc/html/libcoda/arrowdown.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/arrowright.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/bc_s.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/bdwn.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/closed.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/doc.png:
-	$(MAKE) libcoda_doc
+	doc/html/libcoda/modules.html
 doc/html/libcoda/dynsections.js:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/folderclosed.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/folderopen.png:
 	$(MAKE) libcoda_doc
 doc/html/libcoda/group__coda__cursor.html:
 	$(MAKE) libcoda_doc
@@ -62,18 +31,4 @@ doc/html/libcoda/index.html:
 doc/html/libcoda/jquery.js:
 	$(MAKE) libcoda_doc
 doc/html/libcoda/modules.html:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/nav_f.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/nav_g.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/nav_h.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/open.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/splitbar.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/sync_off.png:
-	$(MAKE) libcoda_doc
-doc/html/libcoda/sync_on.png:
 	$(MAKE) libcoda_doc
