99448fad626d9684f30c214d81ce5f827de650aa
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2024-03-14 21:23:15 +0100

    Update doxygen version to 1.10 and swig to 4.2.1

--- docjava.mk
+++ docjava.mk
@@ -18,11 +18,23 @@ JAVA_DOCFILES = \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_product.html \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type-members.html \
 	doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type.html \
+	doc/html/java/clipboard.js \
+	doc/html/java/doc.svg \
+	doc/html/java/docd.svg \
+	doc/html/java/doxygen_crawl.html \
 	doc/html/java/dynsections.js \
+	doc/html/java/folderclosed.svg \
+	doc/html/java/folderclosedd.svg \
+	doc/html/java/folderopen.svg \
+	doc/html/java/folderopend.svg \
 	doc/html/java/functions.html \
 	doc/html/java/functions_func.html \
 	doc/html/java/index.html \
-	doc/html/java/jquery.js
+	doc/html/java/jquery.js \
+	doc/html/java/minus.svg \
+	doc/html/java/minusd.svg \
+	doc/html/java/plus.svg \
+	doc/html/java/plusd.svg
 doc/html/java/annotated.html:
 	$(MAKE) java_doc
 doc/html/java/class_array_ordering_enum.html:
@@ -61,7 +73,23 @@ doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type-members.html:
 	$(MAKE) java_doc
 doc/html/java/classnl_1_1stcorp_1_1coda_1_1_type.html:
 	$(MAKE) java_doc
+doc/html/java/clipboard.js:
+	$(MAKE) java_doc
+doc/html/java/doc.svg:
+	$(MAKE) java_doc
+doc/html/java/docd.svg:
+	$(MAKE) java_doc
+doc/html/java/doxygen_crawl.html:
+	$(MAKE) java_doc
 doc/html/java/dynsections.js:
+	$(MAKE) java_doc
+doc/html/java/folderclosed.svg:
+	$(MAKE) java_doc
+doc/html/java/folderclosedd.svg:
+	$(MAKE) java_doc
+doc/html/java/folderopen.svg:
+	$(MAKE) java_doc
+doc/html/java/folderopend.svg:
 	$(MAKE) java_doc
 doc/html/java/functions.html:
 	$(MAKE) java_doc
@@ -70,4 +98,12 @@ doc/html/java/functions_func.html:
 doc/html/java/index.html:
 	$(MAKE) java_doc
 doc/html/java/jquery.js:
+	$(MAKE) java_doc
+doc/html/java/minus.svg:
+	$(MAKE) java_doc
+doc/html/java/minusd.svg:
+	$(MAKE) java_doc
+doc/html/java/plus.svg:
+	$(MAKE) java_doc
+doc/html/java/plusd.svg:
 	$(MAKE) java_doc
--- doclibcoda.mk
+++ doclibcoda.mk
@@ -1,5 +1,13 @@
 LIBCODA_DOCFILES = \
+	doc/html/libcoda/clipboard.js \
+	doc/html/libcoda/doc.svg \
+	doc/html/libcoda/docd.svg \
+	doc/html/libcoda/doxygen_crawl.html \
 	doc/html/libcoda/dynsections.js \
+	doc/html/libcoda/folderclosed.svg \
+	doc/html/libcoda/folderclosedd.svg \
+	doc/html/libcoda/folderopen.svg \
+	doc/html/libcoda/folderopend.svg \
 	doc/html/libcoda/group__coda__cursor.html \
 	doc/html/libcoda/group__coda__error.html \
 	doc/html/libcoda/group__coda__expression.html \
@@ -9,8 +17,28 @@ LIBCODA_DOCFILES = \
 	doc/html/libcoda/group__coda__types.html \
 	doc/html/libcoda/index.html \
 	doc/html/libcoda/jquery.js \
-	doc/html/libcoda/modules.html
+	doc/html/libcoda/minus.svg \
+	doc/html/libcoda/minusd.svg \
+	doc/html/libcoda/plus.svg \
+	doc/html/libcoda/plusd.svg \
+	doc/html/libcoda/topics.html
+doc/html/libcoda/clipboard.js:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/doc.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/docd.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/doxygen_crawl.html:
+	$(MAKE) libcoda_doc
 doc/html/libcoda/dynsections.js:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/folderclosed.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/folderclosedd.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/folderopen.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/folderopend.svg:
 	$(MAKE) libcoda_doc
 doc/html/libcoda/group__coda__cursor.html:
 	$(MAKE) libcoda_doc
@@ -30,5 +58,13 @@ doc/html/libcoda/index.html:
 	$(MAKE) libcoda_doc
 doc/html/libcoda/jquery.js:
 	$(MAKE) libcoda_doc
-doc/html/libcoda/modules.html:
+doc/html/libcoda/minus.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/minusd.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/plus.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/plusd.svg:
+	$(MAKE) libcoda_doc
+doc/html/libcoda/topics.html:
 	$(MAKE) libcoda_doc
